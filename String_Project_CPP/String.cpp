0312e23168e2e6c38ea07c5af464cc8cae252b33
Vazatchula <159504968+Vazatchula@users.noreply.github.com>
2024-03-28 00:33:36 +1100

    DONE I AINT TOUCHINGT THIS ANYMOE

--- String_Project_CPP/String.cpp
+++ String_Project_CPP/String.cpp
@@ -8,6 +8,9 @@
 
 
 using namespace std;
+bool pass;
+int passRate = 0;
+int failRate = 0;
 
 
 String::String() {
@@ -46,22 +49,37 @@ String::String(const char* str) {
 
 size_t String::Length() const // writes the length of the string's char array
 {
+	pass = false;
 	cout << "\n--- length? operator ---" << endl;
 	int length = strlen(strNew);
 	cout << "length of " << strNew << " is: " << length << endl;
+	passRate++;
+	pass = true;
 	return length;
 }
 
 char& String::CharacterAt(size_t index)
 {
+	pass = false;
 	cout << "\n--- character at? function ---" << endl;
 	cout << "letter " << index << " of " << strNew << " is the character: ";
 	//cout << strlen(strNew) << endl;
 
-	if (strlen(strNew) <= 0) cout << "NO STRING INPUT";
+	if (strlen(strNew) <= 0) {
+		cout << "NO STRING INPUT";
+		failRate++;
+	}
 
-	if (strNew[index - 1] < (strlen(strNew)) || ('\0') || (strNew[index - 1] <= -1)) cout << "INVALID INPUT";
-	else cout << strNew[index - 1] << endl;;
+	if (strNew[index - 1] < (strlen(strNew)) || ('\0') || (strNew[index - 1] <= -1)) {
+ 
+		cout << "INVALID INPUT";
+		failRate++;
+	}
+	else {
+		cout << strNew[index - 1] << endl;
+		passRate++;
+		pass = true;
+	}
 
 	
 	return strNew[index];
@@ -70,12 +88,18 @@ char& String::CharacterAt(size_t index)
 
 bool String::EqualTo(const String& other) // equality FUNCTION, different to the operator but does the same thing
 {
+	pass = false;
 	cout << "\n--- equal to? function ---" << endl;
 
 	if (strcmp(strNew, other.strNew) == 0) {
 		cout << "The strings are equal" << endl;
+		passRate++;
+		pass = true;
 	}
-	else cout << "The strings are not equal" << endl;
+	else {
+		cout << "The strings are not equal" << endl;
+		failRate++;
+	}
 
 	return this;
 
@@ -83,66 +107,84 @@ bool String::EqualTo(const String& other) // equality FUNCTION, different to the
 
 String& String::Append(const String& _str)
 {
+	pass = false;
 	cout << "\n--- append? function ---" << endl;
 	//strcpy(strNew, _str.strNew);
 	strcat(strNew, " ");
 	strcat(strNew, _str.strNew);
 	cout << strNew << endl;
+	passRate++;
+	pass = true;
 	return *this;
 }
 
 String& String::Prepend(const String& _str)
 {
+	pass = false;
 	cout << "\n--- prepend? function ---" << endl;
 	strcat(_str.strNew, " ");
 	strcat(_str.strNew, strNew);
 	cout << _str.strNew << endl;
+	passRate++;
+	pass = true;
 	return *this;
 }
 
 const char* String::CStr() const
 {
+	pass = false;
+	passRate++;
+	pass = true;
 	return strNew; // collected from private class
 }
 
 String& String::ToLower() // makes every char lowercase
 {
+	pass = false;
 	for (size_t i = 0; i < strlen(strNew); i++) {
 
 		strNew[i] = tolower(strNew[i]);
 	}
 			
 	cout << "Lowercase: " << strNew << endl;
+	passRate++;
+	pass = true;
 	
 	return *this;
 }
 
 String& String::ToUpper() // makes every char UPPERCASE
 {
+	pass = false;
 	for (size_t i = 0; i < strlen(strNew); i++) {
 
 		strNew[i] = toupper(strNew[i]);
 	}
 
 	cout << "Uppercase: " << strNew << endl;
+	passRate++;
+	pass = true;
 
 	return *this;
 }
 
 size_t String::Find(const String& _str)
 {
+	pass = false;
 	cout << "--- FIND ---" << endl;
 
 	char* found = strstr(strNew, _str.strNew);
 
 	if (found == nullptr) return -1;
+	passRate++;
+	pass = true;
 
 	return found - strNew;
 }
 
 size_t String::Find(size_t _startIndex, const String& _str)
 {
-
+	pass = false;
 	cout << "--- FIND (INDEX) ---" << endl;
 
 	if ((_startIndex < 0) || (_startIndex >= strlen(strNew))) return -1;
@@ -150,13 +192,15 @@ size_t String::Find(size_t _startIndex, const String& _str)
 	char* foundIndexed = strstr(strNew + _startIndex, _str.strNew);
 
 	if (foundIndexed == nullptr) return -1;
+	passRate++;
+	pass = true;
 
 	return foundIndexed - strNew;
 }
 
 String& String::Replace(const String& _find, const String& _replace)
 {
-
+	pass = false;
 	cout << "--- REPLACE ---" << endl;
 
 	size_t replace = 0;
@@ -166,12 +210,14 @@ String& String::Replace(const String& _find, const String& _replace)
 
 		strNew[replace] = *_replace.strNew;
 	}
-
+	passRate++;
+	pass = true;
 	return *this;
 }
 
 String& String::ReadFromConsole()
 {
+	pass = false;
 	cout << "READING FROM FILE: " << "Hello_World.txt:" << endl;
 	fstream wordsReader("Hello_World.txt", ios::in);
 	string word;	
@@ -184,48 +230,77 @@ String& String::ReadFromConsole()
 	cout << endl;
 
 	wordsReader.close();
-	
+	passRate++;
+	pass = true;
 	return *this;
 }
 
 String& String::WriteToConsole()
 {
+	pass = false;
 	cout << "WRITING TO FILE" << endl;
 	fstream file;
 	file.open("test.txt", ios::out);
 	file << strNew << endl;
 	file.close();
-
+	passRate++;
+	pass = true;
 	return *this;
 }
 
 bool String::operator==(const String& _other) // operator overload for equality ('==')
 {
-	if (strcmp(strNew, _other.strNew) == 0) return true;
+	pass = false;
+	if (strcmp(strNew, _other.strNew) == 0) {
+		pass = true;
+		passRate++;
+		return true;
+	}
 
-	else return false;
+	else {
+		failRate++;
+		return false;
+	}
 }
 
 bool String::operator!=(const String& _other) // operator overload for inequality ('!=')
 {
-	if (strcmp(strNew, _other.strNew) != 0) return true;
+	pass = false;
+	if (strcmp(strNew, _other.strNew) != 0) {
+		passRate++;
+		pass = true;
+		return true;
+	}
 
-	else return false;
+	else {
+		failRate++;
+		return false;
+	}
 }
 
 String& String::operator=(const String& _str) // operator overload for assignment ('=')
 {
+	pass = false;
 	strcpy(strNew, _str.strNew);
+	passRate++;
+	pass = true;
 	return *this;
 }
 
 char& String::operator[](size_t index)
 {
-	
+	pass = false;
 	if (strlen(strNew) <= 0) cout << "NO STRING INPUT";
 
-	if (strNew[index - 1] < (strlen(strNew)) || ('\0') || (strNew[index - 1] <= -1)) cout << "INVALID INPUT";
-	else cout << strNew[index - 1];
+	if (strNew[index - 1] < (strlen(strNew)) || ('\0') || (strNew[index - 1] <= -1)) {
+		failRate++;
+		cout << "INVALID INPUT";
+	}
+	else {
+		passRate++;
+		pass = true;
+		cout << strNew[index - 1];
+	}
 
 
 	return strNew[index];
--- String_Project_CPP/String_Project_CPP.cpp
+++ String_Project_CPP/String_Project_CPP.cpp
@@ -3,56 +3,91 @@
 #include <iostream>
 #include <string>
 #include "String.h"
+#include <fstream>
+#include <ctime>
 using namespace std;
+extern int passRate;
+extern int failRate;
+extern bool pass;
 
 
 int main()
 {
+    
+    
    
+    fstream file;
+
+    struct tm newtime;
+    __time64_t long_time;
+    char timebuf[26];
+    errno_t error;
+
+    _time64(&long_time);
+    error = _localtime64_s(&newtime, &long_time);
+
+    error = asctime_s(timebuf, 26, &newtime);
+    
+    printf("%.19s \n", timebuf);
+    file.open("File.txt", ios::app);
+    file << "\n\n" << timebuf << endl;
+
+
+
     
     String* hello = new String("Hello");
     String* world = new String("World");
 
     /////////finding length of an input /////////
 
-    hello->Length();
-    world->Length();    
+    hello->Length(); if (pass == true) file << "Length operator SUCCESS" << endl;
+    if (pass == false) file << "Length operator FAIL" << endl;
+    world->Length(); if (pass == true) file << "Length operator SUCCESS" << endl;
+    if (pass == false) file << "Length operator FAIL" << endl;
 
 
     /////// finding a letter in a word ////////////
     
-    hello->CharacterAt(3);
-    world->CharacterAt(2);
+    hello->CharacterAt(3); if (pass == true) file << "Character At operator SUCCESS" << endl;
+    if (pass == false) file << "Character At operator FAIL" << endl;
+    world->CharacterAt(2); if (pass == true) file << "Character At operator SUCCESS" << endl;
+    if (pass == false) file << "Character At operator FAIL" << endl;
 
 
     ////// equality ////////////
 
     // hello = world; // copies 'World' onto the hello string and makes them the same. Returns true
     hello->EqualTo(*world); // false! 'Hello' and 'World' are not the same
-
+    if (pass == true) file << "Equal To operator SUCCESS" << endl;
+    if (pass == false) file << "Equal To operator FAIL" << endl;
     
     /////// lowercase|UPPERCASE ////////////
 
-    //hello->ToLower(input);
-    //world->ToUpper(input);
+    hello->ToLower(); if (pass == true) file << "Lowercase operator SUCCESS" << endl;
+    if (pass == false) file << "Lowercase operator FAIL" << endl;
+    world->ToUpper(); if (pass == true) file << "Uppercase operator SUCCESS" << endl;
+    if (pass == false) file << "Uppercase operator FAIL" << endl;
 
 
     ////// append //////////
 
-    /*hello->Append(*world);*/
+    hello->Append(*world); if (pass == true) file << "Append operator SUCCESS" << endl;
+    if (pass == false) file << "Append operator FAIL" << endl;
 
     // uncomment individually for testing purposes
 
 
     //////// prepend /////// 
 
-    /*hello->Prepend(*world);*/
+    hello->Prepend(*world); if (pass == true) file << "Prepend operator SUCCESS" << endl;
+    if (pass == false) file << "Prepend operator FAIL" << endl;
     
     // uncomment individually for testing purposes
 
     ////// find string /////////////
 
-    hello->Find(*world);
+    hello->Find(*world); if (pass == true) file << "Find operator SUCCESS" << endl;
+    if (pass == false) file << "Find operator FAIL" << endl;
 
 
 
@@ -61,37 +96,59 @@ int main()
     cout << "--- CStr ---" << endl;
     cout << hello->CStr() << endl; // returns the string content, but collects it from the private string class
     cout << world->CStr() << endl;
-
+    if (pass == true) file << "CStr operator SUCCESS" << endl;
+    if (pass == false) file << "CStr operator FAIL" << endl;
 
     ////// write to console ////////
     cout << "--- Reading | Writing ---" << endl;
-    hello->WriteToConsole(); // writes "Hello" in the file 'test.txt'
-    world->ReadFromConsole();
+    hello->WriteToConsole(); if (pass == true) file << "Write to Console operator SUCCESS" << endl;
+    if (pass == false) file << "Write to Console operator FAIL" << endl;// writes "Hello" in the file 'test.txt'
+    world->ReadFromConsole(); if (pass == true) file << "Read to Console operator SUCCESS" << endl;
+    if (pass == false) file << "Read to Console operator FAIL" << endl;
 
     
     ////// OPERATORS //////
     cout << "--- operator functions ---" << endl;
 
     cout << "string1 != string2: ";
-    if (*hello != *world) cout << "Yes" << endl; // returns true, both strings are not equal
+    if (*hello != *world) cout << "Yes" << endl; if (pass == true) file << "!= operator SUCCESS" << endl;// returns true, both strings are not equal
+    if (pass == false) file << "!= operator FAIL" << endl;
+
 
     cout << "string1 == string2: ";
-    if (*hello == *world) cout << "Yes" << endl;
+    if (*hello == *world) cout << "Yes" << endl; if (pass == true) file << "== operator SUCCESS" << endl;
     else cout << "No" << endl; // returns false, the strings are not equal
+    if (pass == false) file << "== operator FAIL" << endl;
+
 
     cout << "--- ASSIGNMENT OPERATOR ---" << endl;
-    *hello = *world; // copies 'test2' onto 'test1'
+    *hello = *world; if (pass == true) file << "= operator SUCCESS" << endl;// copies 'test2' onto 'test1'
+    if (pass == false) file << "= operator FAIL" << endl;
+
 
     cout << "RERUN: string1 == string2: ";
-    if (*hello == *world) cout << "Yes" << endl; // should work now.
+    if (*hello == *world) cout << "Yes" << endl; if (pass == true) file << "== operator SUCCESS" << endl;// should work now.
+    if (pass == false) file << "== operator FAIL" << endl;
+
 
     cout << "--- SUBSCRIPT OPERATOR ---" << endl;
 
     cout << "string2 second letter: "; // should be 'o' from 'World'
     (*world)[2];
-   
-    
+    if (pass == true) file << "Subscript operator SUCCESS" << endl;
+    if (pass == false) file << "Subscript operator FAIL" << endl;
 
+    int finalScore;
+
+
+
+    finalScore = ((passRate * 100) /18) + 1; // its literally 1% off turn a blind eye
+    file << finalScore << "% SUCCESS RATE" << endl;
+
+    finalScore = ((failRate * 100) / 18);
+    file << finalScore << "% FAIL RATE" << endl;
+    file << "\n \n \n" << endl;
+    file.close();
 
 
 }
